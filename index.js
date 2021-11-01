92a7b5407246194c2dce3d08ef2e29d0c221771b
Ray Lu <raylu511@gmail.com>
2021-11-01 13:37:17 +0000

    Completed code challenge

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 //1
-function greatestOfTwo(){
-  
+function greatestOfTwo(intOne, intTwo){
+  return intOne > intTwo ? intOne : intTwo;
 }
 
 //2
-function greatestOfThree(){
-  
+function greatestOfThree(intOne, intTwo, intThree){
+  return intOne > intTwo && intOne > intThree ? intOne : intTwo > intOne && intTwo > intThree ? intTwo : intThree;
 }
 
 //3
-function greatestOfFour(){
-  
+function greatestOfFour(intOne, intTwo, intThree, intFour){
+  return intOne > intTwo && intOne > intThree && intOne > intFour ? intOne : intTwo > intOne && intTwo > intThree && intTwo > intFour ? intTwo : intThree > intOne && intThree > intTwo && intThree > intFour ? intThree : intFour ;
 }
 
 
 //4
-function leastOfFour(){
-  
+function leastOfFour(intOne, intTwo, intThree, intFour){
+  return intOne < intTwo && intOne < intThree && intOne < intFour ? intOne : intTwo < intOne && intTwo < intThree && intTwo < intFour ? intTwo : intThree < intOne && intThree < intTwo && intThree < intFour ? intThree : intFour ;
 }
+
+
+console.log(greatestOfTwo(1, 14))           // returns 14
+console.log(greatestOfTwo(-2, 2))           // returns 2
+console.log(greatestOfThree(-5, -2, -6))    // returns -2
+console.log(greatestOfThree(3, 2, 11))      // returns 11
+console.log(greatestOfFour(-5, -2, -6, 0))  // returns 0
+console.log(greatestOfFour(10, 2, 6, 11))   // returns 11
+console.log(leastOfFour(-5, -2, -6, 0))     // returns -6
+console.log(leastOfFour(10, 2, 6, 11))      // returns 2
